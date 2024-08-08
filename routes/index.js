12d09ccf229079295076ea0b83e497add1652bd6
j2131 <166688074+j2131@users.noreply.github.com>
2024-08-08 07:56:58 +0000

    a

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('pg');
+const knex = require('../db/knex');
+const pg = require('pg');
 
 const connection = new pg.Pool({
   host: 'localhost',
@@ -8,39 +9,39 @@ const connection = new pg.Pool({
   password: 'postgres',
   database: 'todo_app',
   port: 5432,
-  });  
+});
 
-  router.get('/', function (req, res, next) {
-    knex("tasks")
-      .select("*")
-      .then(function (results) {
-        console.log(results);
-        res.render('index', {
-          title: 'ToDo App',
-          todos: results,
-        });
-      })
-      .catch(function (err) {
-        console.error(err);
-        res.render('index', {
-          title: 'ToDo App',
-        });
+router.get('/', function (req, res, next) {
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
       });
-  });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
+});
 
-  router.post('/', function (req, res, next) {
-    const todo = req.body.add;
-    knex("tasks")
-      .insert({user_id: 1, content: todo})
-      .then(function () {
-        res.redirect('/')
-      })
-      .catch(function (err) {
-        console.error(err);
-        res.render('index', {
-          title: 'ToDo App',
-        });
+router.post('/', function (req, res, next) {
+  const todo = req.body.add;
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
       });
-  });
+    });
+});
 
 module.exports = router;
